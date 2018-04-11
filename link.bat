bae66839b257dea734dbc278cca5d1aa4cd7f4f7
bluejenga <bluejenga@gmail.com>
2018-04-11 16:29:24 +0900

    link papercolor.vim to its submodule

--- link.bat
+++ link.bat
@@ -1,4 +1,5 @@
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\.vimrc
 mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\.gvimrc
 mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\.vim
+mklink %USERPROFILE%\dotfiles\.vim\colors\PaperColor.vim %USERPROFILE%\dotfiles\papercolor-theme\colors\PaperColor.vim
 pause
