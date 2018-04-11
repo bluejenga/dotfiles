30f533dc103f4ab65b2029386a639ca12dc195ac
bluejenga <bluejenga@gmail.com>
2018-04-11 18:29:35 +0900

    modify bat file to make colors directory

--- link.bat
+++ link.bat
@@ -1,5 +1,6 @@
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\.vimrc
 mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\.gvimrc
 mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\.vim
+mkdir %USERPROFILE%\dotfiles\.vim\colors
 mklink %USERPROFILE%\dotfiles\.vim\colors\PaperColor.vim %USERPROFILE%\dotfiles\papercolor-theme\colors\PaperColor.vim
 pause
